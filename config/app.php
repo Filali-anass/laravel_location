f614fd42fb11769cadacc8d3b07cd3a3cb4e61c2
Filali-anass <anass.filali.93@gmail.com>
2016-06-11 06:41:29 +0000

    Update app.php

--- config/app.php
+++ config/app.php
@@ -147,7 +147,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Collective\Html\HtmlServiceProvider::class,
+        'Illuminate\Html\HtmlServiceProvider'
         
         /*
          * Application Service Providers...
@@ -203,8 +203,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Form'=> Collective\Html\FormFacade::class,
-        'HTML'=> Collective\Html\HtmlFacade::class,
+        'Form'=> 'Illuminate\Html\FormFacade', 
+        'HTML'=> 'Illuminate\Html\HtmlFacade',
     ],
 
 ];
